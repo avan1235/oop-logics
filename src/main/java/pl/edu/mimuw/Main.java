46d45e4985447c662019876e804b695be4b55a32
Pawel Zurakowski <55536129+sommbre@users.noreply.github.com>
2022-05-04 17:22:18 +0200

    implemented logic and solver

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -1,8 +1,44 @@
 package pl.edu.mimuw;
 
+import pl.edu.mimuw.logic.Formula;
+import pl.edu.mimuw.logic.Valuation;
+
+import static pl.edu.mimuw.logic.FormulaFactory.*;
+
 public class Main {
 
   public static void main(String[] args) {
-    throw new IllegalStateException("TODO: show formulas pretty-printed examples");
+    final Formula f1 = iff(
+      or(
+        var("p"),
+        var("q")
+      ),
+      and(
+        not(var("r")),
+        var("q")
+      )
+    );
+
+    System.out.println("Formula f1: " + f1);
+
+    final Valuation val1 = new Valuation();
+    val1.add("p", false);
+    val1.add("q", true);
+    val1.add("r", false);
+
+    System.out.println(val1);
+    System.out.println("f1 evaluates to " + f1.eval(val1));
+
+    final Valuation val2 = new Valuation();
+    val2.add("p", true);
+    val2.add("q", false);
+    val2.add("r", true);
+
+    System.out.println(val2);
+    System.out.println("Now f1 evaluates to " + f1.eval(val2));
+
+    Valuation v = Solver.solve(f1);
+    System.out.println("Valuation found:");
+    System.out.println(v);
   }
 }
--- src/main/java/pl/edu/mimuw/logic/FormulaFactory.java
+++ src/main/java/pl/edu/mimuw/logic/FormulaFactory.java
@@ -5,31 +5,35 @@ public class FormulaFactory {
   private FormulaFactory() {
   }
 
-  public static Formula and(Formula l, Formula formula) {
-    throw new IllegalStateException("TODO");
+  public static Formula and(Formula l, Formula r) {
+    return new And(l, r);
   }
 
-  public static Formula or(Formula l, Formula formula) {
-    throw new IllegalStateException("TODO");
+  public static Formula or(Formula l, Formula r) {
+    return new Or(l, r);
   }
 
-  public static Formula implies(Formula l, Formula formula) {
-    throw new IllegalStateException("TODO");
+  public static Formula implies(Formula l, Formula r) {
+    return new Implies(l, r);
   }
 
-  public static Formula iff(Formula l, Formula formula) {
-    throw new IllegalStateException("TODO");
+  public static Formula iff(Formula l, Formula r) {
+    return new Iff(l, r);
+  }
+
+  public static Formula not(Formula f) {
+    return new Not(f);
   }
 
   public static Formula var(String name) {
-    throw new IllegalStateException("TODO");
+    return new Variable(name);
   }
 
   public static Formula t() {
-    throw new IllegalStateException("TODO");
+    return new Constant(true);
   }
 
   public static Formula f() {
-    throw new IllegalStateException("TODO");
+    return new Constant(false);
   }
 }
